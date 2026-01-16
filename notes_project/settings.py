460a17da16fb52b727aeb4944f111f435f0bddca
rayngoslin <ekhlakov.matvey@gmail.com>
2026-01-16 19:18:51 +0100

    Added CSS to all templates and ensured consistent styling across the application

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.x/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "notes/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.x/ref/settings/#default-auto-field
